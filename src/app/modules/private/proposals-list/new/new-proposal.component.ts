70bc03360797ea1eba3cd1f50b69c7cd062b7fbd
Nicolas Aguilera <naguilera@linq.com>
2023-05-22 16:36:33 -0300

    new updates for new proposal section

--- src/app/modules/private/proposals-list/new/new-proposal.component.ts
+++ src/app/modules/private/proposals-list/new/new-proposal.component.ts
@@ -1,8 +1,35 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-new-proposal',
     templateUrl: './new-proposal.component.html',
     styleUrls: ['./new-proposal.component.scss']
 })
-export class NewProposalComponent {}
+export class NewProposalComponent {
+
+    proposalDescription = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        shortDescription: new FormControl('', [Validators.required]),
+        longDescription: new FormControl('', [Validators.required]),
+    });
+    proposalDetails = new FormGroup({
+        amountNeeded: new FormControl(null, [Validators.required]),
+        estimatedRevenue: new FormControl(null, [Validators.required]),
+        deadline: new FormControl(null, [Validators.required])
+    });
+    proposalLocation = new FormGroup({
+        country: new FormControl('', [Validators.required]),
+        state: new FormControl('', [Validators.required]),
+        city: new FormControl('', [Validators.required]),
+    });
+
+    submitProposal() {
+        const proposal = {
+            ...this.proposalDescription.value,
+            ...this.proposalDetails.value,
+            ...this.proposalLocation.value
+        }
+        console.log(proposal)
+    }
+}
